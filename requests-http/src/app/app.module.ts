f46115302a73e2f81f871bcacae920fcff095e8b
Leonardo <leoenav@gmail.com>
2020-09-30 17:38:38 -0300

    Finish diretivas project and start requests-http

--- requests-http/src/app/app.module.ts
+++ requests-http/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
-import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DiretivaNgifComponent,
-    TestComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
