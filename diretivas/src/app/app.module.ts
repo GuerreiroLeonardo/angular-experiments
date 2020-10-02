d19c3f4c95bc74f0d0e59b03838bf512183eee8e
Leonardo <leoenav@gmail.com>
2020-10-02 14:54:00 -0300

    Adiciona mais diretivas

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 import { TestComponent } from './test/test.component';
+import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
+import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
+import { DiretivaNgclassComponent } from './diretiva-ngclass/diretiva-ngclass.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DiretivaNgifComponent,
-    TestComponent
+    TestComponent,
+    DiretivaNgswitchComponent,
+    DiretivaNgforComponent,
+    DiretivaNgclassComponent
   ],
   imports: [
     BrowserModule,
--- diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
+++ diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-diretiva-ngif',
   templateUrl: './diretiva-ngif.component.html',
-  styleUrls: ['./diretiva-ngif.component.scss']
+  styleUrls: ['./diretiva-ngif.component.scss'],
 })
 export class DiretivaNgifComponent implements OnInit {
+  cursos: string[] = ['Angular 1'];
 
-  constructor() { }
+  mostrarCursos: boolean = true;
 
-  ngOnInit(): void {
+  onMostrarCursos() {
+    this.mostrarCursos = !this.mostrarCursos;
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
